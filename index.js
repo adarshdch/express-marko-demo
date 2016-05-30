7b757501dc98058b1278731965aca2010ea78bca
adarshdch <adarshdch@gmail.com>
2016-05-30 13:55:46 +0530

    Added support for nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.write('Hello World !');
+  res.write('Hello World ! Nodemon is working !');
   res.end();
 });
 
